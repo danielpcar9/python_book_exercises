e545938b6399733b7e247945479115d83c6a5a51
Daniel Palomeque <eldanypalomo@gmail.com>
2025-12-08 20:33:34 -0600

    Refactor exercise 01 to use f strings

--- chapter_01/excercise_01.py
+++ chapter_01/excercise_01.py
@@ -1,5 +1,6 @@
 # Use concatenation to create your full name from your first and last names.
 # The new string should contain your first name, a space, and your last name. 
 # For example, if your name is John Doe, you should combine 'John' and 'Doe' to get 'John Doe'.
-
-print("Daniel" + " " + "Cárdenas")
+first_name = "Daniel"
+last_name = "Cárdenas"
+print(f'My name is: {first_name} {last_name}')
