f51ba56542b16dd78432fad23cda5997eee3ec55
Daniel Palomeque <eldanypalomo@gmail.com>
2025-12-08 20:02:19 -0600

    Add solution to exercise 01: Concatenate first and last name

---
+++ chapter_01/excercise_01.py
@@ -0,0 +1,5 @@
+# Use concatenation to create your full name from your first and last names.
+# The new string should contain your first name, a space, and your last name. 
+# For example, if your name is John Doe, you should combine 'John' and 'Doe' to get 'John Doe'.
+
+print("Daniel" + " " + "Cárdenas")
